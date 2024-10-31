9c78b69019bff77ea85e5357741e7f64fca8ab52
Martijn Halekor <martijn@cardan.com>
2024-10-31 15:57:20 +0100

    update Details/summary component

--- components/Details.vue
+++ components/Details.vue
@@ -1,22 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{ name?: string }>();
+</script>
 
 <template>
-  <details class="details">
+  <details class="details-wrapper" :name="name">
     <summary class="summary">
       <slot name="summary" />
     </summary>
 
-    <slot name="default" />
-    <slot name="details" />
+    <div class="details-content">
+      <slot name="default" />
+    </div>
   </details>
 </template>
 
 <style>
-.summary {
-  cursor: pointer;
+.details-wrapper {
+  background-color: var(--color-card-bg);
+  border: 1px solid var(--color-card-border);
+  border-radius: var(--radius-md);
+  box-shadow: var(--shadow-1);
+  padding-block: 2rem;
+  padding-inline: 1.25rem;
 
-  > * {
-    display: inline-block;
+  .summary {
+    cursor: pointer;
+    margin-inline-start: 1.25rem;
+    list-style-position: outside;
   }
+}
+
+.details-content {
+  padding-block-start: 1rem;
+  padding-inline: 1.25rem;
 }
 </style>
