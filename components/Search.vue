37c9189e36700ed8d126163ecc95c75161ba6887
Martijn Halekor <martijn@cardan.com>
2024-11-20 13:10:05 +0100

    fixes FE-158

--- components/Search.vue
+++ components/Search.vue
@@ -9,20 +9,37 @@ function handleSearch() {
   emit('submit', searchTerm.value);
 }
 
-const emit = defineEmits(['update:model-value', 'submit']);
+function handleReset() {
+  searchTerm.value = undefined;
+  emit('reset', searchTerm.value);
+}
+
+const emit = defineEmits(['update:model-value', 'submit', 'reset']);
 </script>
 
 <template>
   <Form role="search" class="search-form" @submit="handleSearch">
-    <Input
-      v-model="searchTerm"
-      type="search"
-      size="lg"
-      :placeholder="placeholder"
-      :label="$t('general.search')"
-      hide-label
-      @update:model-value="emit('update:model-value', $event)"
-    />
+    <div class="search-input-wrapper">
+      <Input
+        v-model="searchTerm"
+        type="search"
+        size="lg"
+        :placeholder="placeholder"
+        :label="$t('general.search')"
+        hide-label
+        @update:model-value="emit('update:model-value', $event)"
+      />
+
+      <Button
+        type="reset"
+        :label="$t('general.reset')"
+        hide-label
+        icon="refresh"
+        size="sm"
+        variant="ghost"
+        @click="handleReset"
+      />
+    </div>
 
     <Button
       type="submit"
@@ -42,8 +59,27 @@ const emit = defineEmits(['update:model-value', 'submit']);
   gap: 0.25rem;
   grid-template-columns: 1fr auto;
 
+  .form-field-wrapper {
+    width: 100%;
+  }
+
+  .search-input-wrapper {
+    position: relative;
+  }
+
+  input[type='search'] {
+    &::-webkit-search-cancel-button {
+      appearance: none;
+    }
+  }
+
   .button {
-    border-radius: var(--radius-sm);
+    &[type='reset'] {
+      z-index: 1;
+      position: absolute;
+      right: 0.25rem;
+      top: 0.25rem;
+    }
   }
 }
 </style>
