004e1ae1c0f56f90a0bb3b51744caa703c3a3746
Martijn Halekor <martijn@cardan.com>
2024-11-20 13:10:26 +0100

    update colors

--- components/Table.vue
+++ components/Table.vue
@@ -76,7 +76,7 @@ withDefaults(
   }
   th,
   td {
-    padding-block: 0.35rem;
+    padding-block: 0.4rem;
     padding-inline: 0.35rem;
   }
 }
@@ -87,7 +87,7 @@ withDefaults(
   }
   th,
   td {
-    padding-block: 0.4rem;
+    padding-block: 0.5rem;
     padding-inline: 0.5rem;
   }
 }
@@ -98,7 +98,7 @@ withDefaults(
   }
   th,
   td {
-    padding-block: 0.45rem;
+    padding-block: 0.6rem;
     padding-inline: 0.5rem;
   }
 }
