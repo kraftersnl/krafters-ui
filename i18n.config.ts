05d57cdf3e146991c96167aed654482165328d6b
Martijn Halekor <martijn@cardan.com>
2024-11-28 18:58:17 +0100

    add date format option

--- i18n.config.ts
+++ i18n.config.ts
@@ -24,6 +24,14 @@ const datetimeFormats = (): DefineDateTimeFormat => ({
     minute: '2-digit',
     hour12: false,
   },
+  datetimeLong: {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  },
 });
 
 export default defineI18nConfig(() => ({
