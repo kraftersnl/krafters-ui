752ef304fbdf1635ba30d3e3f59c801331301591
Martijn Halekor <martijn@cardan.com>
2024-12-05 17:04:06 +0100

    add isCoarsePointer to useScreenSize

--- app/composables/useScreenSize.ts
+++ app/composables/useScreenSize.ts
@@ -4,6 +4,7 @@ const iPhoneScreen = useMediaQuery(
   'only screen and (min-device-width: 320px) and (max-device-width: 667px)',
 );
 
+const isCoarsePointer = useMediaQuery('(pointer: coarse)');
 const isExtraSmallScreen = useMediaQuery('(min-width: 360px)');
 const isSmallScreen = useMediaQuery('(min-width: 480px)');
 const isMediumScreen = useMediaQuery('(min-width: 768px)');
@@ -13,6 +14,7 @@ const isExtraLargeScreen = useMediaQuery('(min-width: 1280px)');
 export function useScreenSize() {
   return {
     iPhoneScreen,
+    isCoarsePointer,
     isExtraSmallScreen,
     isSmallScreen,
     isMediumScreen,
