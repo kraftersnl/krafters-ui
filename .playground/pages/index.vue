5e52343943cc463969740bef4fd6d48b4d893f09
Martijn Halekor <martijn@cardan.com>
2025-01-22 00:47:39 +0100

    bump version to 1.0.3

--- .playground/pages/index.vue
+++ .playground/pages/index.vue
@@ -6,6 +6,20 @@ useHead({ title: 'Krafters UI' });
   <div class="demo-page changelog-page">
     <h1>Changelog</h1>
 
+    <ChangelogEntry version="1.0.3" date="2025-01-22">
+      <ul>
+        <li>
+          Publish package to NPM
+          <NuxtLink
+            to="https://www.npmjs.com/package/@krafters/ui"
+            target="_blank"
+            external
+            ><code>@krafters/ui</code></NuxtLink
+          >
+        </li>
+      </ul>
+    </ChangelogEntry>
+
     <ChangelogEntry version="1.0.2" date="2025-01-20">
       <ul>
         <li>Add <code>position</code> prop to Dialog component</li>
