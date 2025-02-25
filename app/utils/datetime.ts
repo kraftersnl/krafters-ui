8b0db786778156c9e74e71440b179fc8edb6aed2
Martijn Halekor <martijn@cardan.com>
2025-02-25 23:09:14 +0100

    update datetime helper

--- app/utils/datetime.ts
+++ app/utils/datetime.ts
@@ -1,12 +1,16 @@
-export function datetime(date?: number | string | Date, locale = 'en') {
-  if (!date) return;
-
-  return new Date(date).toLocaleDateString(locale, {
+export function datetime(
+  date: number | string | Date,
+  locale = 'en',
+  format: Intl.DateTimeFormatOptions = {
     day: 'numeric',
     month: 'long',
     year: 'numeric',
     hour: '2-digit',
     minute: '2-digit',
     hour12: false,
-  });
+  },
+) {
+  if (!date) return;
+
+  return new Date(date).toLocaleDateString(locale, format);
 }
