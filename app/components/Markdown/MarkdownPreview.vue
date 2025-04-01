a8aa098c86021c57fea540e5891e7ddc5a8c3931
Martijn Halekor <martijn@cardan.com>
2025-04-02 01:57:33 +0200

    enable linkify in MarkdownPreview

--- app/components/Markdown/MarkdownPreview.vue
+++ app/components/Markdown/MarkdownPreview.vue
@@ -15,8 +15,9 @@ hljs.registerLanguage('css', css);
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('typescript', typescript);
 
-const options = {
+const md = markdownit({
   html: true,
+  linkify: true,
   highlight: (str, lang) => {
     if (lang && hljs.getLanguage(lang)) {
       try {
@@ -32,13 +33,12 @@ const options = {
 
     return '<pre><code class="hljs">' + escapeHTML(str) + '</code></pre>';
   },
-};
+});
 
-const md = markdownit(options)
-  .use(targetBlankExtension)
-  .use(italicExtension)
-  .use(attrs)
-  .use(mark);
+md.use(targetBlankExtension);
+md.use(italicExtension);
+md.use(attrs);
+md.use(mark);
 
 const props = defineProps<{
   content: string;
@@ -73,20 +73,26 @@ const preview = computed(() => md.render(props.content));
     hyphens: none;
   }
 
-  a[target='_blank']::after {
-    content: '';
-    display: inline-block;
-    vertical-align: middle;
-    width: 14px;
-    height: 14px;
-    margin-left: 4px;
-    background-color: currentColor;
-    -webkit-mask-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6"/><path d="m21 3-9 9"/><path d="M15 3h6v6"/></svg>');
-    mask-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6"/><path d="m21 3-9 9"/><path d="M15 3h6v6"/></svg>');
-    -webkit-mask-size: contain;
-    mask-size: contain;
-    -webkit-mask-repeat: no-repeat;
-    mask-repeat: no-repeat;
+  a {
+    &:hover {
+      color: var(--color-accent);
+    }
+
+    &[target='_blank']::after {
+      content: '';
+      display: inline-block;
+      vertical-align: middle;
+      width: 14px;
+      height: 14px;
+      margin-left: 4px;
+      background-color: currentColor;
+      -webkit-mask-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6"/><path d="m21 3-9 9"/><path d="M15 3h6v6"/></svg>');
+      mask-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6"/><path d="m21 3-9 9"/><path d="M15 3h6v6"/></svg>');
+      -webkit-mask-size: contain;
+      mask-size: contain;
+      -webkit-mask-repeat: no-repeat;
+      mask-repeat: no-repeat;
+    }
   }
 }
 </style>
