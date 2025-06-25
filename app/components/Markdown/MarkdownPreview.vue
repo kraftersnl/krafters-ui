1e646ab037e3b8f2623717a9b40be0f4a980b0e5
Martijn Halekor <martijn@cardan.com>
2025-06-25 15:18:14 +0200

    use new solar icon for links that open in new window in MarkdownPreview

--- app/components/Markdown/MarkdownPreview.vue
+++ app/components/Markdown/MarkdownPreview.vue
@@ -116,9 +116,9 @@ const preview = computed(() => md.render(content));
       vertical-align: middle;
       width: 1em;
       height: 1em;
-      margin-left: 0.2em;
+      margin-inline: 0.25em 0.1em;
       background-color: var(--color-grey-text);
-      mask-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 20 20" fill="currentColor"><path d="M11 3a1 1 0 1 0 0 2h2.586l-6.293 6.293a1 1 0 1 0 1.414 1.414L15 6.414V9a1 1 0 1 0 2 0V4a1 1 0 0 0-1-1z"/><path d="M5 5a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-3a1 1 0 1 0-2 0v3H5V7h3a1 1 0 0 0 0-2z"/></svg>');
+      mask-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="1.5"><path d="M13 22c4.055-.008 6.178-.107 7.535-1.465C22 19.072 22 16.714 22 12s0-7.071-1.465-8.536C19.071 2 16.714 2 12 2S4.929 2 3.464 3.464C2.107 4.822 2.008 6.944 2 11"/><path stroke-linejoin="round" d="m3 21l8-8m0 0H5m6 0v6"/></g></svg>');
       mask-size: contain;
       mask-repeat: no-repeat;
     }
