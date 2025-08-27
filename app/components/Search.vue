3211b3c5a7d19ec8855275cb2018d10864b636f1
Martijn Halekor <martijn@cardan.com>
2025-08-27 16:17:19 +0200

    update styles for Search component

--- app/components/Search.vue
+++ app/components/Search.vue
@@ -59,7 +59,7 @@ const emit = defineEmits<{
         v-model="model"
         type="search"
         :size="size"
-        :icon="hideLabel ? 'material-symbols:search-rounded' : undefined"
+        :icon="hideSubmitButton ? 'material-symbols:search-rounded' : undefined"
         :placeholder="placeholder"
         :label="label || $t('general.search')"
         :hide-label="hideLabel"
@@ -94,8 +94,9 @@ const emit = defineEmits<{
 
 <style>
 .search-form {
+  --search-color: var(--focus-color);
   --column-gap: 0rem;
-  position: relative;
+
   flex-grow: 1;
   flex-basis: 240px;
   display: grid;
@@ -107,15 +108,22 @@ const emit = defineEmits<{
   }
 
   .form-field-wrapper {
+    position: relative;
     width: 100%;
+
+    .input {
+      border-start-end-radius: 0;
+      border-end-end-radius: 0;
+    }
   }
 
   .search-input-wrapper {
     display: flex;
+    position: relative;
   }
 
   input[type='search'] {
-    padding-inline-end: 4.25rem;
+    border-inline-end: 0;
 
     &::-webkit-search-cancel-button {
       appearance: none;
@@ -124,76 +132,77 @@ const emit = defineEmits<{
 
   .button {
     &[type='submit'] {
-      position: absolute;
-      z-index: 1;
       right: 0;
       bottom: 0;
       color: var(--color-white);
-      background-color: var(--color-text);
-      border-color: var(--color-text);
+      background-color: var(--search-color);
+      border-color: var(--search-color);
       min-width: 2rem;
       border-end-start-radius: 0;
       border-start-start-radius: 0;
-      margin-inline-start: -1px;
+      border-inline-start: 0;
       outline-offset: -1px;
       transition-duration: 0s;
 
-      &:focus-visible {
-        background-color: var(--color-black);
-        outline-offset: 1px;
-        z-index: 1;
+      .iconify {
+        transition-duration: 0s;
       }
 
-      &:disabled {
-        opacity: 1;
-        color: var(--color-grey-graphic);
-        background-color: var(--color-grey-bg);
+      &:focus {
+        color: var(--color-white);
+        background-color: var(--search-color);
+        outline-offset: 1px;
       }
     }
 
     &[type='reset'] {
       z-index: 1;
       position: absolute;
-      right: 2.75rem;
+      right: 0.25rem;
       bottom: 0.25rem;
+      outline-offset: -1px;
     }
   }
 
-  &:has(input[type='search']:focus-visible) {
+  &:has(input[type='search']:focus) {
     .button[type='submit'] {
-      border-color: var(--focus-color);
-
-      &:not(:disabled) {
-        background-color: var(--focus-color);
-      }
+      color: var(--color-white);
+      border-color: var(--search-color);
+      background-color: var(--search-color);
+      outline: 2px solid var(--focus-color);
     }
   }
 
   .input-size--sm {
     + .button[type='reset'] {
       bottom: 0.25rem;
-      right: 2.25rem;
+      right: 0.25rem;
+    }
+
+    input[type='search'] {
+      padding-inline-end: 1.75rem;
     }
   }
+
   .input-size--md {
     + .button[type='reset'] {
       bottom: 0.375rem;
-      right: 2.5rem;
+      right: 0.375rem;
+    }
+
+    input[type='search'] {
+      padding-inline-end: 2rem;
     }
   }
+
   .input-size--lg {
     + .button[type='reset'] {
       bottom: 0.25rem;
-      right: 2.75rem;
+      right: 0.325rem;
     }
-  }
-}
 
-.dark-mode {
-  .search-form {
-    .button[type='submit'] {
-      background-color: var(--color-grey-text);
-      border-color: var(--color-grey-text);
+    input[type='search'] {
+      padding-inline-end: 2.5rem;
     }
   }
 }
