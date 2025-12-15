b23affa395459d1db910e47893721ea6cec10ff7
Martijn Halekor <martijn@cardan.com>
2025-12-15 11:31:54 +0100

    remove any type from useCookieDialog

--- app/composables/useCookiesDialog.ts
+++ app/composables/useCookiesDialog.ts
@@ -42,7 +42,7 @@ export function useCookiesDialog() {
   });
 
   function gtag() {
-    gtm.proxy.dataLayer.push(arguments as any);
+    gtm.proxy.dataLayer.push(arguments);
   }
 
   onMounted(() => {
